b6c1a32556301e129b511db9296838fb288c2a2f
KristianMyzeqari <kristian.myzeqari@mail.mcgill.ca>
2024-11-13 13:55:47 -0500

    mainCookBookId made optional

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client'
+import { PrismaService } from '../prisma/prisma.service';
+import { UserCreateInput } from '../@generated/user/user-create.input';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService){}
+
+    async createUser(data: UserCreateInput){
+        return this.prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                username: data.username,
+                password: data.password,
+                recipes: data.recipes,
+                cookbooks: data.cookbooks
+
+            }
+        });
+    }
+}
