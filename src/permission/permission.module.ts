7292ba92219a06d13c73868b45d533f526dfbd49
unknown <nicholas.murphy2@mail.mcgill.ca>
2024-11-26 13:04:29 -0500

    30 -- First implementation of create permission

--- src/permission/permission.module.ts
+++ src/permission/permission.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PermissionResolver } from './permission.resolver';
 import { PermissionService } from './permission.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [PermissionResolver, PermissionService]
+  providers: [PrismaService, PermissionResolver, PermissionService]
 })
 export class PermissionModule {}
--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { PermissionCreateInput } from '../@generated/permission/permission-create.input';
+import { PermissionLevel, ResourceType } from '@prisma/client';
 
 @Injectable()
-export class PermissionService {}
+export class PermissionService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createPermission(data: PermissionCreateInput) {
+        try {
+            if (!data.userId || !data.resourceId || !data.permissionLevel || !data.resourceType) {
+                throw new BadRequestException('Resource ID, resource type, user ID and permission level are required fields for permission creation');
+            }
+            //ensure user exists in database
+            await this.prisma.user.findUnique({where: {id: data.userId}});
+            //ensure permission doesn't already exist in database
+            const existingPermission = await this.prisma.permission.findFirst({
+                where: {
+                    userId: data.userId,
+                    resourceId: data.resourceId,
+                    resourceType: data.resourceType,
+                }
+            });
+            if (existingPermission) {
+                throw new Error('Permission already exists. Use editPermission instead.');
+            }
+            //create permission in database
+            return this.prisma.permission.create({ data });
+        } catch (error) {
+            throw error;
+        }
+    }
+}
