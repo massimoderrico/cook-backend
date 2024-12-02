7e4765569cb13a205a22bdd0f20cf840a6419546
unknown <nicholas.murphy2@mail.mcgill.ca>
2024-12-01 21:07:01 -0500

    37 -- duplicate recipe implementation, tested manually

--- src/recipe/recipe.resolver.ts
+++ src/recipe/recipe.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Int } from '@nestjs/graphql';
 import { CreateOneRecipeArgs } from 'src/@generated/recipe/create-one-recipe.args';
 import { Recipe } from 'src/@generated/recipe/recipe.model';
 import { RecipeService } from './recipe.service';
@@ -7,12 +7,25 @@ import { RecipeCreateInput } from 'src/@generated/recipe/recipe-create.input';
 @Resolver(() => Recipe)
 export class RecipeResolver {
     constructor(private readonly recipeService: RecipeService) {}
+    
     @Mutation(() => Recipe, {nullable: true})
     async createRecipe(@Args('data') recipe: RecipeCreateInput): Promise<Recipe> {
-    try{
-        return this.recipeService.createRecipe(recipe);
-    } catch(err){
-        throw err;
+        try{
+            return this.recipeService.createRecipe(recipe);
+        } catch(err){
+            throw err;
+        }
     }
+
+    @Mutation(() => Recipe, { nullable: true })
+    async duplicateRecipe(
+        @Args('recipeId', { type: () => Int }) recipeId: number,
+        @Args('newUserId', { type: () => Int }) newUserId: number,
+    ): Promise<Recipe> {
+        try {
+            return await this.recipeService.duplicateRecipe(recipeId, newUserId);
+        } catch (error) {
+            throw new Error(`Failed to duplicate recipe: ${error.message}`);
+        }
     }
 }
