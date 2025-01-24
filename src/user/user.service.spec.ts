0599796f1cf550d4cc9565f1838a14b939e6bc8b
unknown <nicholas.murphy2@mail.mcgill.ca>
2025-01-24 13:54:52 -0500

    4 -- implemented update profile image functionality for user, and unit tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -356,4 +356,49 @@ describe('UserService', () => {
       });
     });
   });
+
+  describe('changePictureUser', () => {
+    it('should change the user\'s picture successfully', async () => {
+      const userId = 1;
+      const newImage = 'not null';
+      const mockUser: User = {
+        id: userId,
+        name: 'Old Name',
+        email: 'test@example.com',
+        username: 'testuser',
+        password: 'hashedpassword',
+        mainCookbookId: null,
+        role: 'USER',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        image: "null",
+      };
+      const updatedUser: User = { ...mockUser, name: newImage };
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUser);
+      jest.spyOn(prisma.user, 'update').mockResolvedValue(updatedUser);
+      const result = await service.changePictureUser(userId, newImage);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({ where: { id: userId } });
+      expect(prisma.user.update).toHaveBeenCalledWith({
+        where: { id: userId },
+        data: { image: newImage },
+      });
+      expect(result).toEqual(updatedUser);
+    });
+
+    it('should throw BadRequestException if user does not exist', async () => {
+      const userId = 1;
+      const newImage = 'mock';
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+      await expect(service.changePictureUser(userId, newImage)).rejects.toThrow(
+        `User with ID 1 does not exist`
+      );
+    });
+
+    it('should throw an error if prisma throws an exception', async () => {
+      const userId = 1;
+      const newImage = 'mock';
+      jest.spyOn(prisma.user, 'findUnique').mockRejectedValue(new Error('Some internal error'));
+      await expect(service.changePictureUser(userId, newImage)).rejects.toThrow('Some internal error');
+    });
+  });
 });
