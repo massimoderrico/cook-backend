0f58a6d0ef7c685c47d0ff827de5766e72a6dc66
massderrico <massderrico@yahoo.com>
2025-03-03 17:09:19 -0500

    protected all routes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { SignInResponse } from './dto/signin.input';
 import { SignUpInput } from './dto/signin.input';
 import { LoginInput } from './dto/login.input';
+import { Public } from './public.decorator';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Mutation(() => SignInResponse)
   async login(@Args('data') loginData: LoginInput): Promise<SignInResponse> {
     return this.authService.login(loginData);
   }
 
+  @Public()
   @Mutation(() => SignInResponse)
   async signup(@Args('data') signupData: SignUpInput): Promise<SignInResponse> {
     return this.authService.signup(signupData);
