acc8cca238a366c19f3df38af7069539550786f6
massderrico <massderrico@yahoo.com>
2025-03-03 19:04:13 -0500

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
 
   // Apply global JWT guard
   const reflector = app.get(Reflector);
